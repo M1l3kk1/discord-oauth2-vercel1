e1ef0ede305a8c92308fd82a7fcd437eb7d8afae
M1l3kk1 <mlkkm1329@gmail.com>
2025-07-05 19:33:06 +0200

    Update dziekujemy.js

--- pages/dziekujemy.js
+++ pages/dziekujemy.js
@@ -1,1 +1,8 @@
-
+export default function Dziekujemy() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20vh", fontFamily: "sans-serif" }}>
+      <h1>Dziękujemy za weryfikację!</h1>
+      <p>Możesz już zamknąć tę stronę lub wrócić na serwer.</p>
+    </div>
+  );
+}
