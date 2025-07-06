0bd21a103fed653db81a7814200abc1a8d6da9ac
M1l3kk1 <mlkkm1329@gmail.com>
2025-07-06 15:30:17 +0200

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,12 +1,10 @@
-import clientPromise from "../../lib/mongodb";
-
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 export default async function handler(req, res) {
   const code = req.query.code;
 
   if (!code) {
-    return res.status(400).send("Brak kodu autoryzacyjnego");
+    return res.status(400).json({ error: "Brak kodu autoryzacyjnego" });
   }
 
   const CLIENT_ID = process.env.CLIENT_ID;
@@ -23,51 +21,31 @@ export default async function handler(req, res) {
   });
 
   try {
-    // Wymiana kodu na token
     const tokenRes = await fetch("https://discord.com/api/oauth2/token", {
       method: "POST",
       body: data,
-      headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
     });
 
     if (!tokenRes.ok) {
       const errorText = await tokenRes.text();
-      return res.status(500).send(`Błąd tokenu: ${errorText}`);
+      return res.status(500).json({ error: "Błąd tokenu", details: errorText });
     }
 
     const tokenJson = await tokenRes.json();
     const access_token = tokenJson.access_token;
 
-    // Pobranie danych użytkownika
     const userRes = await fetch("https://discord.com/api/users/@me", {
-      headers: { Authorization: `Bearer ${access_token}` }
+      headers: {
+        Authorization: `Bearer ${access_token}`
+      }
     });
 
     const userData = await userRes.json();
-
-    // Połączenie z bazą
-    const client = await clientPromise;
-    const db = client.db("vercel-discord");
-
-    // Zapis lub aktualizacja użytkownika
-    await db.collection("users").updateOne(
-      { id: userData.id },
-      { $set: userData },
-      { upsert: true }
-    );
-
-    // Odpowiedź HTML z podziękowaniem
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.status(200).send(`
-      <html>
-        <head><title>Dziękujemy za weryfikację!</title></head>
-        <body style="font-family:sans-serif; text-align:center; margin-top:50px;">
-          <h1>Dziękujemy, ${userData.username}!</h1>
-          <p>Twoja weryfikacja przebiegła pomyślnie.</p>
-        </body>
-      </html>
-    `);
+    return res.status(200).json({ user: userData });
   } catch (error) {
-    res.status(500).json({ error: "Wewnętrzny błąd serwera", details: error.message });
+    return res.status(500).json({ error: "Wewnętrzny błąd serwera", details: error.message });
   }
 }
